cf472ac011596269e1b2dc8e7146d8f75c83ac87
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-11-17 12:27:09 -0600

    Basic Statements Exercise - Working on Pt 1

---
+++ albums_seeder.sql
@@ -0,0 +1,26 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist_name, record_name, release_date, sales_mil, genre)
+VALUES
+
+('Led Zeppelin', 'Led Zeppelin IV', 1971, 29.0, 'Hard rock, Heavy metal, Folk'),
+('Michael Jackson', 'Bad', 1987, 20.7, 'Pop, R&B, Funk, Rock'),
+('Alanis Morissette', 'Jagged Little Pill', 1995, 24.3, 'Alternative rock'),
+('Michael Jackson', 'Dangerous', 1991, 17.0, 'New jack swing, R&B, pop'),
+
+('Adele', '21', 2011, 26.4, 'Pop, soul'),
+('Celine  Dion', 'Lets Talk About Love', 1997, 19.3, 'Pop, soft rock'),
+('Metallica', 'Metallica', 1991, 25.2, 'Metallica'),
+('The Beatles', '1', 2000, 23.5, 'Rock'),
+('The Beatles', 'Abbey Road', 1969, 14.4, 'Rock'),
+('ABBA', 'Gold: Greatest Hits', 1992, 23.0, 'Pop, disco'),
+('Bruce Springsteeen', 'Born in the USA', 1984, 19.6, 'Rock'),
+('Madonna', 'The Immaculate Collection', 1990, 19.5, 'Pop, dance'),
+('Pink Floyd', 'The Wall', 1979, 18.7, 'Progressive rock'),
+
+('James Horner', 'Titanic: Music from the Motion Picture', 1997, 18.1, 'Soundtrack'),
+('Dire Straits', 'Brothers in Arms', 1985, 17.7, 'Rock'),
+('Nirvana', 'Nevermind', 1991, 16.7, 'Grunge, alternative rock'),
+('Santana', 'Supernatural', 1999, 20.5, 'Latin Rock'),
+('Guns N Roses', 'Appetite for Destruction', 1987, 21.9, 'Heavy metal, hard rock'),
+('Elton John', 'Goodbye Yellow Brick Road', 1973, 8.5, 'rock, pop rock, glam rock');
