f14e3907d50577d7d1998fb6fcb308274d679284
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-11-17 10:07:10 -0600

    Finished Tables Exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,6 +1,8 @@
 USE codeup_test_db;
 
-CREATE TABLE IF NOT EXISTS quotes (
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums (
                                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                                       artist_name VARCHAR(50) DEFAULT 'NONE',
                                       record_name  VARCHAR(100) NOT NULL,
